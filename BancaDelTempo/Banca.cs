688f5955160a9a5f936c10048f053749b887056f
ghila05 <nicolaghi05@gmail.com>
2023-08-07 17:03:06 +0200

    correzioni classi

--- BancaDelTempo/Banca.cs
+++ BancaDelTempo/Banca.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace BancaDelTempo
 {
-    internal class Banca
+    public class Banca : IEquatable<Banca>
     {
+        string nome, zona, id;
+
+        public string Nome { get; set; }
+        public string Zona { get; set; }
+        public string Id { get; private set;}
+
+        private Utente u = new Utente();
+        public Banca() { 
+        
+        }
+
+        public Banca(string id, string nome, string zona) { 
+            this.id = id;
+            this.nome = nome;
+            this.zona = zona;
+        }
+
+
+        public override string ToString()
+        {
+            return nome + ";" + zona;
+        }
+        public bool Equals(Banca b) {
+            if (b == null) return false;
+            if (this == b) return true;
+            return (this.id == b.id);
+        }
+
+
+
     }
 }
--- BancaDelTempo/Utente.cs
+++ BancaDelTempo/Utente.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace BancaDelTempo
 {
-    internal class Utente
+    public class Utente : IEquatable<Utente>
     {
+        string id, nome, cognome, tel;
+        float debiti = 0;
+
+        public string Id { get; private set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Tel { get; set; }
+        public float Debiti { get; set; }
+        public Utente()
+        {
+
+        }
+        public Utente(string nome, string cognome, string tel) { 
+            id = CreaId(nome, cognome, tel);
+            this.nome = nome;
+            this.cognome = cognome;
+            this.tel = tel;
+        }
+
+        public string CreaId(string nome, string cognome, string tel) {
+            string id;
+            id = nome.Substring(0,4);
+            id = id + cognome.Substring(0, 3);
+            id = id + tel.Substring(6, 3);
+            return id;
+        
+        }
+
+        public override string ToString()
+        {
+            return id + ";" + nome + ";" + cognome + ";" + tel + ";" + debiti;
+        }
+
+        public bool Equals(Utente p)
+        {
+            if (p == null) return false;
+            if (this == p) return true;
+            return (this.id == p.id);
+        }
+
     }
 }
